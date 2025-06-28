6bbded7963787c573ab5deb91a6226ad38a3bd8c
badabarro <badaoudoubarro73@gmail.com>
2025-06-28 16:54:16 +0200

    Pipeline for Terraform Project

--- terraform-ec2-auto/keyPair.tf
+++ terraform-ec2-auto/keyPair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "dev-key" {
   key_name   = "dev-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGAj5RILLa+KYbvTmdmqaBDQJ1L5y99O6m6AODqUR5x9 Admin@DESKTOP-4UKNA42"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnqwFBdysqMr/n9pwJ8XLHFV+GaqhaLNLQPt7pPOzBv Admin@DESKTOP-4UKNA42"
 }
