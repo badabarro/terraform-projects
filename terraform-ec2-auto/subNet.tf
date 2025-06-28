9fefef2e3d8fcffa0241cf9950900555316a3938
badabarro <badaoudoubarro73@gmail.com>
2025-06-28 17:09:16 +0200

    Pipeline for Terraform-Ec2

---
+++ terraform-ec2-auto/subNet.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
